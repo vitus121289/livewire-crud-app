abfcef679f3defef989e449b345a6fa4ff2b0078
vitus121289 <vmtusing@gmail.com>
2022-09-15 21:55:37 +0800

    Used the form error component, and removed the wire:model to make this more flexible.

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -2,12 +2,9 @@
 <div>
     <label for="{{ $name }}">{{ ucwords(str_replace('_', ' ', $name)) }}</label>
     <textarea
-        wire:model="{{ $name }}"
         name="{{ $name }}"
         id="{{ $name }}"
         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
     ></textarea>
-    @error($name)
-        {{ $message }}
-    @enderror
+    <x-forms.error name="{{ $name }}" />
 </div>
