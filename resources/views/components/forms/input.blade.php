1b199353ea59380fe9dedf5f1ecab88b7f3c47ed
vitus121289 <vmtusing@gmail.com>
2022-09-15 21:57:08 +0800

    Used the form error component, and removed the wire:model to make this more flexible.

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -2,13 +2,10 @@
 <div>
     <label for="{{ $name }}">{{ ucwords(str_replace('_', ' ', $name)) }}</label>
     <input
-        wire:model="{{ $name }}"
         name="{{ $name }}"
         id="{{ $name }}"
-        class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+        class="w-full px-4 py-2 my-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
         {{ $attributes }}    
     >
-    @error($name)
-        {{ $message }}
-    @enderror
+    <x-forms.error name="{{ $name }}" />
 </div>
