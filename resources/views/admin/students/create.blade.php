24eb165c75b9e3774e6dc8f5af120499a69012a9
vitus121289 <vmtusing@gmail.com>
2022-09-15 22:02:08 +0800

    Edited to make it compatible with the changes to the student-form livewire component.

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -1,4 +1,15 @@
 <x-app-layout>
-    @livewire('admin.student-form')
+    <x-slot name="header">
+        Add Student
+    </x-slot>
+    <x-forms.layout
+        action="/admin/students"
+        method="post"
+        enctype="multipart/form-data"
+    >
+        @csrf
+        @livewire('admin.student-form')
+        <x-forms.button>Add Student</x-forms.button>
+    </x-forms.layout>
     @livewireScripts
 </x-app-layout>
