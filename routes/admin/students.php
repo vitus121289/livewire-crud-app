e5d66ba37f1ed111a59ad7ef9f986defbf9c3216
vitus121289 <vmtusing@gmail.com>
2022-09-15 21:48:10 +0800

    Created routes for: show, create, store, edit, update, and destroy. Also, used a middleware so that only authorized admins can access these routes.

--- routes/admin/students.php
+++ routes/admin/students.php
@@ -3,5 +3,11 @@
 use App\Http\Controllers\Admin\StudentController as AdminStudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/students/add', [AdminStudentController::class, 'create'])->name('add-student');
-Route::post('admin/students/', [AdminStudentController::class, 'store']);
+Route::middleware(['auth', 'can:admin'])->group(function() {
+    Route::get('admin/students', [AdminStudentController::class, 'index'])->name('list-students');
+    Route::get('admin/students/add', [AdminStudentController::class, 'create'])->name('add-student');
+    Route::get('admin/students/{student}', [AdminStudentController::class, 'edit'])->name('edit-student');
+    Route::post('admin/students/', [AdminStudentController::class, 'store']);
+    Route::patch('admin/students/{student}', [AdminStudentController::class, 'update']);
+    Route::delete('admin/students/{student}', [AdminStudentController::class, 'destroy']);
+});
