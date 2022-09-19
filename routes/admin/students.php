587e6ab302f98a27d277129a03552017c8477c38
vitus121289 <vmtusing@gmail.com>
2022-09-19 19:10:01 +0800

    Added a middleware property that requires the user to verify their email before they can access this route.

--- routes/admin/students.php
+++ routes/admin/students.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\StudentController as AdminStudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'can:admin'])->group(function() {
+Route::middleware(['auth', 'can:admin', 'verified'])->group(function() {
     Route::get('admin/students', [AdminStudentController::class, 'index'])->name('list-students');
     Route::get('admin/students/add', [AdminStudentController::class, 'create'])->name('add-student');
     Route::get('admin/students/{student}', [AdminStudentController::class, 'edit'])->name('edit-student');
