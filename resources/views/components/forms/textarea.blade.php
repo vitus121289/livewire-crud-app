d28c53db9e794f8939de4fa6db0c337053cd706d
vitus121289 <vmtusing@gmail.com>
2022-09-19 19:13:02 +0800

    Added a blade directive for accepting additional attributes.

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -5,6 +5,7 @@
         name="{{ $name }}"
         id="{{ $name }}"
         class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+        {{ $attributes }}
     ></textarea>
     <x-forms.error name="{{ $name }}" />
 </div>
