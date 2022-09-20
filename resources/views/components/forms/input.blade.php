50ff5123945d69198f2d2ab03ff3fb9984a7d697
vitus121289 <vmtusing@gmail.com>
2022-09-20 11:39:54 +0800

    Removed whitespace after the attributes.

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -5,7 +5,7 @@
         name="{{ $name }}"
         id="{{ $name }}"
         class="w-full px-4 py-2 my-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-        {{ $attributes }}    
+        {{ $attributes }}
     >
     <x-forms.error name="{{ $name }}" />
 </div>
