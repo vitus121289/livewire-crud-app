c171ac2e1ea4240ac71fd36ea2d091a6cefa9ee0
vitus121289 <vmtusing@gmail.com>
2022-09-20 11:42:56 +0800

    Added an if block to show a flash message when the session has a fail flag.

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -8,3 +8,13 @@
         <p>{{ session('success') }}</p>
     </div>
 @endif
+@if (session()->has('fail'))
+    <div
+        x-data="{ show:true }"
+        x-init="setTimeout(() => show = false, 4000)"
+        x-show="show"
+        class="fixed bg-red-500 text-white py-2 px-4 rounded-xl bottom-3 right-3 sm"
+    >
+        <p>{{ session('fail') }}</p>
+    </div>
+@endif
