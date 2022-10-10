93ca71f5825d6ba7f02c3d1b12b29f0619974850
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:21:29 +0800

    Removed unncessary lines of code.

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -2,14 +2,6 @@
     <x-slot name="header">
         Add Student
     </x-slot>
-    <x-forms.layout
-        action="/admin/students"
-        method="post"
-        enctype="multipart/form-data"
-    >
-        @csrf
-        @livewire('admin.student-form')
-        <x-forms.button>Add Student</x-forms.button>
-    </x-forms.layout>
+    @livewire('admin.student-form')
     @livewireScripts
 </x-app-layout>
