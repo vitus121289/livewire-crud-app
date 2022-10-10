ab8c5d4792e6c25068a3c8666790a4da0e59c1f4
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:21:00 +0800

    Removed unnecessary lines of code.

--- resources/views/admin/students/edit.blade.php
+++ resources/views/admin/students/edit.blade.php
@@ -2,23 +2,8 @@
     <x-slot name="header">
         Edit Student
     </x-slot>
-    <x-forms.layout
-        action="/admin/students/{{ $student->id }}"
-        method="post"
-        enctype="multipart/form-data"
-    >
-        @csrf
-        @method('PATCH')
-        @livewire('admin.student-form', [
-            'first_name' => $student->first_name,
-            'middle_name' => $student->middle_name,
-            'last_name' => $student->last_name,
-            'birthdate' => $student->birthdate,
-            'photo' => $student->photo,
-            'section' => $student->section,
-            'address' => $student->address
+    @livewire('admin.student-form', [
+            'student' => $student
             ])
-        <x-forms.button>Edit Student</x-forms.button>
-    </x-forms.layout>
     @livewireScripts
 </x-app-layout>
