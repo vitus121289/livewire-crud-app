5a2ab29388d5c5dd00864dcbefc82a07841e0ed9
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:12:59 +0800

    Added styling for this component's slot.

--- resources/views/components/tables/header-column.blade.php
+++ resources/views/components/tables/header-column.blade.php
@@ -2,5 +2,7 @@
     class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
     {{ $attributes }}
 >
-    {{ $slot }}
+    <div class="flex items-center">
+        {{ $slot }}
+    </div>
 </th>
