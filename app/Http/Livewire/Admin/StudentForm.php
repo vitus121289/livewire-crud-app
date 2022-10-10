c70b38aed4594489207b977e95a0c81a9468f357
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:27:12 +0800

    Implemented data model binding, created the rules method used for validation, created the mount method with a logic that determines the value of the method variable, created a method for realtime validation of the photo field, created a method for storing student records, created a method for updating records, and created a method that checks if the student being added already exists.

--- app/Http/Livewire/Admin/StudentForm.php
+++ app/Http/Livewire/Admin/StudentForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Student;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -9,30 +10,72 @@ class StudentForm extends Component
 {
     use WithFileUploads;
 
-    public $first_name;
-    public $middle_name;
-    public $last_name;
-    public $birthdate;
+    public Student $student;
+    public $method;
     public $photo;
-    public $section;
-    public $address;
 
-    protected $rules = [
-        'first_name' => 'required|min:2|max:255',
-        'middle_name' => 'required|min:2|max:255',
-        'last_name' => 'required|min:2|max:255',
-        'birthdate' => 'required|date',
-        'photo' => 'image',
-        'section' => 'max:255',
-        'address' => 'required|min:6|max:255'
-    ];
+    public function rules() {
+        return [
+                'student.first_name' => 'required|min:2|max:255',
+                'student.middle_name' => 'required|min:2|max:255',
+                'student.last_name' => 'required|min:2|max:255',
+                'student.birthdate' => 'required|date',
+                'photo' => $this->method === 'store' ? 'required|image' : 'nullable',
+                'student.section' => 'max:255',
+                'student.address' => 'required|min:6|max:255'
+            ];
+    }
+
+    public function mount($student = null) {
+        if (request()->routeIs('add-student')) {
+            $this->student = new Student();
+            $this->method = 'store';
+        }
+
+        if (request()->routeIs('edit-student')) {
+            $this->student = $student;
+            $this->method = 'update';
+        }
+    }
 
     public function updated($propertyName) {
         $this->validateOnly($propertyName);
     }
 
+    public function updatedPhoto() {
+        $this->validate();
+    }
+
+    public function store() {
+        if ($this->checkForDuplicateFullname()) {
+            session()->flash('fail', 'Student alread exists.');
+        } else {
+            $this->validate();
+            $this->student->photo = $this->photo->store('student_ids');
+            $this->student->save();
+            session()->flash('success', 'Added student');
+            redirect()->to('/admin/students');
+        }
+    }
+
+    public function update() {
+        $this->validate();
+        if (! is_null($this->photo)) {
+            $this->student->photo = $this->photo->store('student_ids');
+        }
+        $this->student->update();
+        session()->flash('success', 'Edited Student');
+    }
+
     public function render()
     {
         return view('livewire.admin.student-form');
+    }
+
+    private function checkForDuplicateFullname() {
+        return Student::where('first_name', $this->student->first_name)
+                        ->where('middle_name', $this->student->middle_name)
+                        ->where('last_name', $this->student->last_name)
+                        ->exists();
     }
 }
