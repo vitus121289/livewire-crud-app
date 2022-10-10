2663952a57d58d49c167a21db93b0bd9e25bc674
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:23:30 +0800

    Removed deprecated methods. These deprecated methods now can be found in the StudentForm (storing, and updating), and StudentTable (deleting) livewire components.

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -9,45 +9,17 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     public function index() {
-        return view('admin.students.index', [
-            'students' => Student::latest()->paginate(20)
-        ]);
+        return view('admin.students.index');
     }
 
     public function create() {
         return view('admin.students.create');
     }
-
-    public function store() {
-        $attributes = $this->validateFields();
-
-        if ($this->checkForDuplicateFullname()) {
-            return back()->with('fail', 'Student already exists');
-        } else {
-            $attributes['photo'] = request()->file('photo')->store('student_ids');
-
-            Student::create($attributes);
-
-            return redirect('admin/students')->with('success', 'Added student.');
-        }
-    }
-
+    
     public function edit(Student $student) {
         return view('admin.students.edit', [
             'student' => $student
         ]);
-    }
-
-    public function update(Student $student) {
-        $attributes = $this->validateFields($student);
-
-        if (isset($attributes['photo'])) {
-            $attributes['photo'] = request()->file('photo')->store('student_ids');
-        }
-
-        $student->update($attributes);
-
-        return redirect('admin/students')->with('success', 'Student updated.');
     }
 
     public function destroy(Student $student) {
@@ -55,24 +27,5 @@ class StudentController extends Controller
         $student->delete();
 
         return back()->with('success', 'Student deleted.');
-    }
-
-    private function validateFields(?Student $student = null) {
-        return request()->validate([
-            'first_name' => ['required', 'min:2', 'max:255'],
-            'middle_name' => ['required', 'min:2', 'max:255'],
-            'last_name' => ['required', 'min:2', 'max:255'],
-            'birthdate' => ['required', 'date'],
-            'photo' => is_null($student) ? ['required', 'image'] : 'image',
-            'section' => 'max:255',
-            'address' => ['required', 'min:6', 'max:255']
-        ]);
-    }
-
-    private function checkForDuplicateFullname() {
-        return Student::where('first_name', request()->first_name)
-                        ->where('middle_name', request()->middle_name)
-                        ->where('last_name', request()->last_name)
-                        ->exists();
     }
 }
