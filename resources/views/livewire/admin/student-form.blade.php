a49afa6be9f0e6e1fefc0ef13ba7f2fa2489f8cc
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:41:59 +0800

    Modified to comply with data model binding, provided logic to preview photo ID of the student being edited, provided logic to preview photo ID of student being created, and modified so that this livewire component can be used for adding and editing students.

--- resources/views/livewire/admin/student-form.blade.php
+++ resources/views/livewire/admin/student-form.blade.php
@@ -1,15 +1,24 @@
-<div class="mt-4">
-    <x-forms.input name="first_name" type="text" wire:model="first_name" />
-    <x-forms.input name="middle_name" type="text" wire:model="middle_name" />
-    <x-forms.input name="last_name" type="text" wire:model="last_name" />
-    <x-forms.input name="birthdate" type="date" wire:model="birthdate" />
-    <x-forms.input name="photo" type="file" wire:model="photo" />
-    @if (isset($photo) && request()->routeIs('edit-student'))
-        <div class="flex-shrink-0 w-20 h-20 m-4">
-            <img src="{{ asset('storage/' . $photo) }}" alt="" />
-        </div>
-    @endif
-    <x-forms.textarea name="section" wire:model="section"></x-forms.textarea>
-    <x-forms.textarea name="address" wire:model="address"></x-forms.textarea>
-    {{-- <x-forms.button type="submit">Add Student</x-forms.button> --}}
-</div>
+<x-forms.layout wire:submit.prevent="{{ $method }}" enctype="multipart/form-data">
+    <div class="mt-4">
+        <x-forms.input name="student.first_name" type="text" wire:model="student.first_name" />
+        <x-forms.input name="student.middle_name" type="text" wire:model="student.middle_name" />
+        <x-forms.input name="student.last_name" type="text" wire:model="student.last_name" />
+        <x-forms.input name="student.birthdate" type="date" wire:model="student.birthdate" />
+        {{-- <x-forms.input name="student.photo" type="file" wire:model="student.photo" /> --}}
+        <x-forms.input name="photo" type="file" wire:model="photo" />
+        @if ($photo)
+            <div class="flex-shrink-0 w-20 h-20 m-4">
+                <img src="{{ $photo->temporaryUrl() }}">
+            </div>
+        @endif
+        @if ((isset($student->photo) && ($method === 'update')) && (!$photo))
+            <div class="flex-shrink-0 w-20 h-20 m-4">
+                <img src="{{ asset('storage/' . $student->photo) }}" alt="" />
+            </div>
+        @endif
+        <x-forms.textarea name="student.section" wire:model="student.section"></x-forms.textarea>
+        <x-forms.textarea name="student.address" wire:model="student.address"></x-forms.textarea>
+        <x-forms.button type="submit">{{ ($method === 'update') ? 'Edit Student' : 'Add Student' }}</x-forms.button>
+    </div>
+    <x-flash />
+</x-forms.layout>
