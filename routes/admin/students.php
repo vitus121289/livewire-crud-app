add9514af750defa9b224e3a022ae499940eb11a
vitus121289 <vmtusing@gmail.com>
2022-10-10 19:06:21 +0800

    Removed the routes for storing, updating, and deleting a student record because it is already being handled by the StudentForm livewire component.

--- routes/admin/students.php
+++ routes/admin/students.php
@@ -7,7 +7,4 @@ Route::middleware(['auth', 'can:admin', 'verified'])->group(function() {
     Route::get('admin/students', [AdminStudentController::class, 'index'])->name('list-students');
     Route::get('admin/students/add', [AdminStudentController::class, 'create'])->name('add-student');
     Route::get('admin/students/{student}', [AdminStudentController::class, 'edit'])->name('edit-student');
-    Route::post('admin/students/', [AdminStudentController::class, 'store']);
-    Route::patch('admin/students/{student}', [AdminStudentController::class, 'update']);
-    Route::delete('admin/students/{student}', [AdminStudentController::class, 'destroy']);
 });
